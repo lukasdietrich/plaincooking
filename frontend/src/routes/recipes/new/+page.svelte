b6ce9e53fed599c6031ee2cc68a5d7e59d421f88
Lukas Dietrich <lukas@lukasdietrich.com>
2024-03-18 00:04:14 +0100

    feat: Add new recipe

--- frontend/src/routes/recipes/new/+page.svelte
+++ frontend/src/routes/recipes/new/+page.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import { goto } from '$app/navigation';
 	import { resolveRoute } from '$app/paths';
-	import { page } from '$app/stores';
 	import { client } from '$lib';
 	import { t } from '$lib/i18n';
 	import { ActionPortal, Action } from '$lib/components/actions';
 	import { Editor } from '$lib/components/editor';
 
-	export let data: PageData;
-	let content = data.content;
+	let content = '';
 
 	async function save() {
-		const recipeId = $page.params.recipeId;
-
-		await client.recipes.updateRecipe(recipeId, content);
-		await goto(resolveRoute('/recipes/[recipeId]', $page.params), { invalidateAll: true });
+		const  { id } = await client.recipes.createRecipe(content);
+		await goto(resolveRoute('/recipes/[recipeId]', { recipeId: id }), { invalidateAll: true });
 	}
 </script>
 
 <ActionPortal>
-	<Action href={resolveRoute('/recipes/[recipeId]', $page.params)} title={$t('actions.back')}>
+	<Action href="/" title={$t('actions.back')}>
 		<i class="icon-undo-2"></i>
 	</Action>
 
