9777e4f60535dcd5dde41951cb4ecfe2eda23ec6
Lukas Dietrich <lukas@lukasdietrich.com>
2024-03-17 17:47:26 +0100

    chore: Update chunkSizeWarningLimit because codemirror is a large dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ import unocss from '@unocss/svelte-scoped/vite';
 
 export default defineConfig({
 	plugins: [unocss(), sveltekit()],
+
+	build: {
+		// The only chunk larger than 500kB contains the codemirror editor.
+		// It is already code-split properly and only loaded when navigating to the edit page.
+		chunkSizeWarningLimit: 768,
+	},
+
 	server: {
 		proxy: {
 			'/api': 'http://localhost:8080'
