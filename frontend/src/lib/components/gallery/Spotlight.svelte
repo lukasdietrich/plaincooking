d4fcb08c41e2d8af33588d4b3e3594f8a54937d6
Lukas Dietrich <lukas@lukasdietrich.com>
2024-03-29 18:16:12 +0100

    fix: Improve accessibility to close fullscreen images

--- frontend/src/lib/components/gallery/Spotlight.svelte
+++ frontend/src/lib/components/gallery/Spotlight.svelte
@@ -1,38 +1,23 @@
 <script lang="ts">
 	import type { components } from '$lib/api';
-	import { onMount, createEventDispatcher } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+	import { scale } from 'svelte/transition';
 
 	export let image: components['schemas']['AssetMetadata'];
 
 	const dispatch = createEventDispatcher();
-
-	let loading = true;
-	let mounted = false;
-
-	onMount(() => setTimeout(() => (mounted = true), 10));
-
-	$: hidden = loading || !mounted;
 
 	function handleClose() {
 		dispatch('close');
 	}
 </script>
 
-<div
-	class="fixed inset-0 z-750 transition-300 bg-black/90 flex items-center justify-center"
-	class:opacity-0={hidden}
+<button
+	class="fixed inset-0 z-750 bg-black/90 flex items-center justify-center"
+	on:click={handleClose}
+	transition:scale={{ start: 1.05, duration: 200, delay: 150 }}
 >
 	<div class="w-full h-full p-15 rounded-10">
-		<img
-			src={image.href}
-			alt={image.id}
-			on:load={() => (loading = false)}
-			class="mx-auto h-full object-scale-down"
-		/>
+		<img src={image.href} alt={image.id} class="mx-auto h-full object-scale-down" />
 	</div>
-
-	<button class="absolute right-5 top-5 rounded text-white text-lg" on:click={handleClose}>
-		<i class="icon-x bg-white/30 backdrop-blur-10 p-2 rounded-full transition hover:bg-red-700/80"
-		></i>
-	</button>
-</div>
+</button>
