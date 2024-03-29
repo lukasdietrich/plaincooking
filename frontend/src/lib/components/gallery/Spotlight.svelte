60ebb34f31ac628367c65349b8c0554dca76eafc
Lukas Dietrich <lukas@lukasdietrich.com>
2024-03-29 21:53:09 +0100

    feat: Wait for fullscreen image to load before displaying it

--- frontend/src/lib/components/gallery/Spotlight.svelte
+++ frontend/src/lib/components/gallery/Spotlight.svelte
@@ -7,17 +7,26 @@
 
 	const dispatch = createEventDispatcher();
 
+	$: preload = new Promise<HTMLImageElement>((resolve) => {
+		const preload = new Image();
+
+		preload.src = image.href;
+		preload.addEventListener('load', () => resolve(preload));
+	});
+
 	function handleClose() {
 		dispatch('close');
 	}
 </script>
 
-<button
-	class="fixed inset-0 z-750 bg-black/90 flex items-center justify-center"
-	on:click={handleClose}
-	transition:scale={{ start: 1.05, duration: 200, delay: 150 }}
->
-	<div class="w-full h-full p-15 rounded-10">
-		<img src={image.href} alt={image.id} class="mx-auto h-full object-scale-down" />
-	</div>
-</button>
+{#await preload then image}
+	<button
+		class="fixed inset-0 z-750 bg-black/90 flex items-center justify-center"
+		on:click={handleClose}
+		transition:scale={{ start: 1.05, duration: 200, delay: 150 }}
+	>
+		<div class="w-full h-full p-15 rounded-10">
+			<img src={image.src} alt={image.alt} class="mx-auto h-full object-scale-down" />
+		</div>
+	</button>
+{/await}
